5138ff9729242cf87ba9e35abb31415d6d7da8af
Andres Kalle <andres@play.ee>
2021-03-29 16:12:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Initial setup
+* In `node` dir: `npm install`
+* In root dir: `docker-compose build`
+
+## Starting/stopping the cluster
+* Start: `docker-compose up -d --scale replica=2 --scale sentinel=3`
+* Stop: `docker-compose down`
+
+## Executing the test
+* The cluster must be running
+* In one terminal, start the client script: `docker-compose run --rm nodejs node node/client.js`
+* Note the IP of the current master that SentinelConnector resolves
+  * Alternatively, get it via `docker-compose exec sentinel redis-cli -p 26379 sentinel get-master-addr-by-name mymaster`
+* Wait for it to make a few requests
+* In another terminal, initiate failover: `docker-compose exec redis-cli redis-cli -h <ip of current master> debug sleep 30`
+  * Be sure to replace the IP parameter
+* The client script should detect the failover in 6-8 seconds and reconnect
+* Stop the client script
+* Comment out the `await listenForMasterSwitch(sentinels, client)` line
+* Restart the client script
+* Initiate failover again, but with the new master IP
+* The client script should hang for 30 seconds, send a few requests to the old master and then reconnect to the new one
---
+++ node/client.js
@@ -0,0 +1,104 @@
+process.env.DEBUG = 'ioredis:SentinelConnector'
+
+const dns = require('dns')
+
+const Redis = require('ioredis')
+
+// Avoid hanging on Ctrl-C when running via docker-compose
+process.on('SIGINT', () => process.exit())
+
+const run = async () => {
+  const ips = await getIps()
+  printIps(ips)
+
+  const sentinels = [
+    { host: ips.sentinel_1, port: 26379 },
+    { host: ips.sentinel_2, port: 26379 },
+    { host: ips.sentinel_3, port: 26379 },
+  ]
+
+  const client = initClient(sentinels)
+
+  // Comment this out during the test
+  await listenForMasterSwitch(sentinels, client)
+
+  startRequests(client)
+}
+
+const getIps = async () => {
+  const ips = {
+    'redis-main': '',
+    replica_1: '',
+    replica_2: '',
+    sentinel_1: '',
+    sentinel_2: '',
+    sentinel_3: '',
+  }
+
+  await Promise.all(Object.keys(ips).map(async (hostname) => {
+    const result = await dns.promises.lookup(hostname)
+    ips[hostname] = result.address
+  }))
+
+  return ips
+}
+
+const printIps = (ips) => {
+  for (const [hostname, ip] of Object.entries(ips)) {
+    console.log((hostname + ':').padStart(12), ip)
+  }
+}
+
+const initClient = (sentinels) => {
+  const client = new Redis({ sentinels, name: 'mymaster' })
+
+  client.on('ready', () => console.log(time(), 'ready'))
+  client.on('reconnecting', () => console.log(time(), 'reconnecting'))
+  client.on('close', () => console.log(time(), 'close'))
+  client.on('error', () => console.log(time(), 'error'))
+
+  return client
+}
+
+const listenForMasterSwitch = async (sentinels, client) => {
+  let lastMasterIp = ''
+  let lastMasterPort = ''
+
+  for (const { host, port } of sentinels) {
+    const sentinelClient = new Redis({ host, port })
+    await sentinelClient.subscribe('+switch-master')
+
+    sentinelClient.on('message', (_channel, message) => {
+      // message example: 'mymaster 172.26.0.4 6379 172.26.0.3 6379'
+      const [masterName, oldIp, oldPort, newIp, newPort] = message.split(' ')
+
+      if (newIp !== lastMasterIp || newPort !== lastMasterPort) {
+        lastMasterIp = newIp
+        lastMasterPort = newPort
+
+        // This would call stream.end() which wouldn't trigger a reconnection
+        // redis.disconnect(true)
+
+        console.log(time(), 'Got switch-master message from sentinel, disconnecting client')
+        client.connector.stream.destroy()
+      }
+    })
+  }
+}
+
+const startRequests = (client) => {
+  let i = 0
+
+  setInterval(async () => {
+    i += 1
+    const reqId = i
+
+    console.log(time(), 'request', reqId)
+    const value = await client.incr('foo')
+    console.log(time(), 'response', reqId + ':', value)
+  }, 5000)
+}
+
+const time = () => new Date().toISOString()
+
+run().catch(console.error)
